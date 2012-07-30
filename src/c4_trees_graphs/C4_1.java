083497a5c11578207f09b34d58d488e6566480ad
Fengjiang Li <fengjial@andrew.cmu.edu>
2012-07-29 17:24:11 -0700

    reform

---
+++ src/c4_trees_graphs/C4_1.java
@@ -0,0 +1,13 @@
+package c4_trees_graphs;
+
+/**
+ * Given a binary search tree, design an algorithm which creates a linked
+ * list of all the nodes at each depth (eg, if you have a tree with depth D, 
+ * you will have D linked lists)
+ *
+ * @author Fengjiang.
+ *         Created Jul 29, 2012.
+ */
+public class C4_1 {
+
+}
